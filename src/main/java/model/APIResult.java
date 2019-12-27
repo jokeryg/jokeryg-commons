5e763e85f5292310df5bd9eb093a51937b103e5b
jokerYG <smallblood@gmail.com>
2019-12-27 22:53:11 +0800

    封装APIResult几个常用方法，并且默认是成功

--- src/main/java/model/APIResult.java
+++ src/main/java/model/APIResult.java
@@ -1,6 +1,7 @@
 package model;
 
 import com.google.gson.Gson;
+import enums.APICode;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,15 +18,44 @@ import java.lang.reflect.Type;
 public class APIResult<T> {
     private static final Gson gson = new Gson();
 
-    private int responseCode = 0;//返回码,默认成功
+    private int responseCode = APICode.成功.getCode();//返回码,默认成功
     private String errorMsg;//错误时错误信息描述
     private String token;//新的token
     private T result;//返回对象
 
+    public void setResponseCode(APICode apiCode) {
+        this.responseCode = apiCode.getCode();
+    }
 
-    public APIResult(int responseCode, String errorMsg){
-        this.responseCode = responseCode;
+    /**
+     * 业务参数错误
+     * @param errorMsg 错误信息描述
+     */
+    public void paramError(String errorMsg){
+        error(APICode.业务参数错误,errorMsg);
+    }
+
+    /**
+     * 返回错误
+     * @param apiCode 返回码
+     * @param errorMsg 错误信息描述
+     */
+    public void error(APICode apiCode,String errorMsg){
+        setAll(apiCode,errorMsg,null,null);
+    }
+
+    /**
+     * 设置所有字段
+     * @param apiCode 返回码
+     * @param errorMsg 错误时错误信息描述
+     * @param token
+     * @param result 返回对象
+     */
+    public void setAll(APICode apiCode,String errorMsg,String token,T result){
+        this.responseCode = apiCode.getCode();
         this.errorMsg = errorMsg;
+        this.result = result;
+        this.token = token;
     }
 
     /**
